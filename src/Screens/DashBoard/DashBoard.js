786e2530a9a06b1cecf7d41339dbf446b7b74203
Chek_Fire <chekkoriamine123@gmail.com>
2019-02-26 15:11:54 +0100

    Dshboard_V1

---
+++ src/Screens/DashBoard/DashBoard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavigationBar from "../../components/NavigationBar/NavigationBar";
+import DashboardMenu from "../../components/DashboardMenu/DashBoardMenu";
+export default class DashBoard extends Component {
+  render() {
+    return (
+      <div style={{width:'100%'}}>
+        <div style={{width:'100%'}}>
+        <NavigationBar />
+        </div>
+        <div style={{width:'100%'}}>
+          <DashboardMenu />
+        </div>
+      </div>
+    )
+  }
+}
